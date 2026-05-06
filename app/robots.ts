2a8a277e6896faf1d2ab8d371796a5ebfe7cc020
Akawat Moradsatian - 6609681231 <144298634+Akawatmor@users.noreply.github.com>
2026-05-06 15:01:00 +0700

    fix robot

--- app/robots.ts
+++ app/robots.ts
@@ -6,10 +6,36 @@ export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "Google-Extended",
+        disallow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "CCBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "Bytespider",
+        disallow: "/",
+      },
+      {
+        userAgent: "Amazonbot",
+        disallow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: absoluteUrl("/sitemap.xml"),
     host: getSiteUrl(),
   };
